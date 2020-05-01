db8b8a5655d82d0f317dc086d94290f7f52a548c
eogud7126 <eogud7126@gmail.com>
2020-05-01 21:49:01 +0900

    kakao 스터디

---
+++ Algorithm/200430_programmers_kakao(불량사용자).cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+
+#include<set>
+
+using namespace std;
+
+vector<string> user_id = { "frodo", "fradi", "crodo", "abc123", "frodoc" };
+vector<string> banned_id = { "*rodo", "*rodo", "******" };
+vector<string> island[8];
+int answer = 0;
+
+bool visited[8] = { false, };
+set<set<string>> setAnswer;	//중복검사를 위한것.
+
+void DFS(int idx, set<string> s) {
+	for (int i = 0;i < island[idx].size();i++) {
+		set<string> tmp = s;
+		tmp.insert(island[idx][i]);
+		if (tmp.size() == idx) continue;
+		if (tmp.size() == banned_id.size()) {
+			setAnswer.insert(tmp);
+		}
+		if (idx<banned_id.size())
+			DFS(idx + 1, tmp);
+	}
+}
+int solution(vector<string> user_id, vector<string> banned_id) {
+	for (int i = 0;i < banned_id.size();i++) {
+		for (int j = 0;j < user_id.size();j++) {
+			bool flag = true;
+			if (banned_id[i].size() != user_id[j].size())
+				continue;
+			for (int k = 0;k < user_id[j].size();k++) {
+				if (banned_id[i][k] == '*') continue;
+				if (user_id[j][k] != banned_id[i][k]) {
+					flag = false;
+					break;
+				}
+			}
+			//모든 문자가 일치한다면
+			if (flag) {
+				island[i].push_back(user_id[j]);
+			}
+		}
+	}
+	set<string> s;
+	DFS(0,s);
+	answer = setAnswer.size();
+	return answer;
+}
+
+int main(void) {
+	cout << solution(user_id, banned_id) << endl;
+	return 0;
+}
---
+++ Algorithm/200501_programmers_kakao(자물쇠와 열쇠).cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<stack>
+
+#include<set>
+
+using namespace std;
+
+vector<vector<int>> key = { {1,1},{1,1} };
+vector<vector<int>> lock = { {1, 0, 0},{1, 0, 0},{1, 1, 1} };
+
+//90도 시계방향 회전
+vector<vector<int>> rotateKey(vector<vector<int>> key) {
+	vector<vector<int>> newKey(key.size(),vector<int>(key[0].size())); //NXN 공간만들기
+	for (int i = 0;i < key.size();i++) {
+		for (int j = key[0].size() - 1;j >= 0;j--) {
+			newKey[i][key.size()-1 - j] = key[j][i];
+		}
+	}
+	return newKey;
+}
+bool checkIsunlock(vector<vector<int>> lock, int N, int M) {
+	for (int i = 0;i < M;i++) {
+		for (int j = 0;j < M;j++) {
+			if (lock[i + N][j + N] == 0) 
+				return false;
+		}
+	}
+	//cout << "★★★★★★★★★★★풀렸당★★★★★★★★★★★★" << endl;
+	return true;
+}
+
+bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+	bool answer = false;
+	int N = key.size();
+	int M = lock.size();
+	vector<vector<int>> newLock(2 * N + M, vector<int>(2 * N + M));
+	for (int i = 0;i < M;i++) {
+		for (int j = 0;j < M;j++) {
+			newLock[N + i][N + j] = lock[i][j];
+		}
+	}
+	vector<vector<int>> tmp(2 * N + M, vector<int>(2 * N + M));
+	for (int i = 0;i < 4;i++) {//3해주니깐 틀림
+		for (int m = 0;m < N + M;m++) {
+			for (int n = 0;n < N + M;n++) {
+				tmp = newLock;
+				bool flag1 = true;
+				for (int p = 0;p < N;p++) {
+					bool flag2 = true;
+					for (int q = 0;q < N;q++) {
+						if (tmp[p+m][q+n] == 0)
+							tmp[p+m][q+n] = key[p][q];
+						else { //1인경우 겹치는지 확인
+							if (key[p][q] == 1) {
+								flag2 = false;
+								flag1 = false;
+								break;
+							}
+						}
+					}
+					if (!flag2) {
+						break;
+					}
+				}
+				for (int i = 0;i < tmp.size();i++) {
+					for (int j = 0;j < tmp.size();j++)
+						cout << tmp[i][j];
+					cout << endl;
+				}
+				cout << endl;
+				if (flag1)
+					if (checkIsunlock(tmp, N, M)) {
+						answer = true;
+						break;
+					}
+			}
+			if (answer) break;
+		}
+		if (answer) break;
+		key = rotateKey(key);
+		//cout << "================회전===============" << endl;
+	}
+
+
+	return answer;
+}
+
+int main(void) {
+	cout << solution(key,lock) << endl;
+	return 0;
+}
